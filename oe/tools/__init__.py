4d5dbb04529f881f2fe656e7bee66d5c6d95f71f
eos <wolfuardian@gmail.com>
2023-10-09 16:23:00 +0800

    initialize project
    ocms-editor-0.2310.0004-unstable

--- oe/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-from oe.tools.maya import Maya
-from oe.tools.registry import Registry
-from oe.tools.logging import Logging
-from oe.tools.packages import Packages
-from oe.tools.io import IO
-from oe.tools.xml import XML
-from oe.tools.string import String
